fc512178bab76e0f535d91c0a132930fd0a729e3
Aleksander Afanasyev <afonia.marcii@gmail.com>
2023-12-15 19:42:25 +0300

    update: comparison section

--- src/components/home/ComparisonHomeInfo.vue
+++ src/components/home/ComparisonHomeInfo.vue
@@ -26,9 +26,79 @@
     </div>
     <h2 class="home-comparison__label"><span class="home-comparison__label__gold">95% магазинов</span> заказывают мебель
       из Китая и не только в России, но весь остальной мир!</h2>
-    <div>
-      <div></div>
-      <div></div>
+    <div class="home-comparison__main-block">
+
+      <div class="main-block__buy-info">
+        <h4 class="buy-info__label">Онлайн покупка мебели из РФ, Европы и Китая</h4>
+        <ul class="buy-info__list-info">
+          <li class="list-info__elem"><img class="elem__icon" src="/assets/icons/home/cross-icon.svg" alt="cross icon.">
+            <p class="elem__text">Ограниченный выбор на сайтах. Большинство фабрик РФ предлагает 5-10 стандартных
+              однообразных моделей.</p>
+          </li>
+          <li class="list-info__elem"><img class="elem__icon" src="/assets/icons/home/cross-icon.svg" alt="cross icon.">
+            <p class="elem__text">Нельзя оценить заранее качество изделий и материалов.</p>
+          </li>
+          <li class="list-info__elem"><img class="elem__icon" src="/assets/icons/home/cross-icon.svg" alt="cross icon.">
+            <p class="elem__text">Большой риск получить мебель, не соответствующую ожиданиям.</p>
+          </li>
+          <li class="list-info__elem"><img class="elem__icon" src="/assets/icons/home/cross-icon.svg" alt="cross icon.">
+            <p class="elem__text">Приходится слепо доверять отзывам о производителе или посреднике.</p>
+          </li>
+          <li class="list-info__elem"><img class="elem__icon" src="/assets/icons/home/cross-icon.svg" alt="cross icon.">
+            <p class="elem__text">Необходимо заказывать мебель, сантехнику и свет из разных магазинов и переплачивать за
+              доставку.</p>
+          </li>
+          <li class="list-info__elem"><img class="elem__icon" src="/assets/icons/home/cross-icon.svg" alt="cross icon.">
+            <p class="elem__text">Известные и надежные европейские бренды на сегодняшний день недоступны к прямому заказу
+              из РФ.</p>
+          </li>
+          <li class="list-info__elem"><img class="elem__icon" src="/assets/icons/home/cross-icon.svg" alt="cross icon.">
+            <p class="elem__text">Переплата от 30 до 60% если вы покупаете у дистрибьютора, а не у производителя напрямую.
+            </p>
+          </li>
+        </ul>
+        <img class="buy-info__img" src="/assets/images/home/sofa-comparison-1.png"
+          alt="sofa with magazine table and armchair.">
+      </div>
+      <div class="main-block__buy-info main-block__buy-info__trips">
+        <h4 class="buy-info__label">Мебельный тур в Китай с РосЭкспресс</h4>
+        <ul class="buy-info__list-info">
+          <li class="list-info__elem"><img class="elem__icon" src="/public/assets/icons/home/pick-icon.svg"
+              alt="pick icon.">
+            <p class="elem__text">Прямой доступ к фабрикам-производителям и тысячам вариантов качественной мебели.</p>
+          </li>
+          <li class="list-info__elem"><img class="elem__icon" src="/public/assets/icons/home/pick-icon.svg"
+              alt="pick icon.">
+            <p class="elem__text">Доступ к эксклюзивным коллекциям мебели, не выставленным на продажу онлайн.</p>
+          </li>
+          <li class="list-info__elem"><img class="elem__icon" src="/public/assets/icons/home/pick-icon.svg"
+              alt="pick icon.">
+            <p class="elem__text">Вы лично удостоверитесь в высоком качестве: оцените материалы и рассмотрите каждую
+              деталь.</p>
+          </li>
+          <li class="list-info__elem"><img class="elem__icon" src="/public/assets/icons/home/pick-icon.svg"
+              alt="pick icon.">
+            <p class="elem__text">Проверите мебель в реальных условиях. Сможете сесть на стулья, раскрыть ящики комода,
+              разложить диван.</p>
+          </li>
+          <li class="list-info__elem"><img class="elem__icon" src="/public/assets/icons/home/pick-icon.svg"
+              alt="pick icon.">
+            <p class="elem__text">Купите в ходе тура всё необходимое для комплектации интерьера под ключ.</p>
+          </li>
+          <li class="list-info__elem"><img class="elem__icon" src="/public/assets/icons/home/pick-icon.svg"
+              alt="pick icon.">
+            <p class="elem__text">Погрузитесь в атмосферу, традиции и уникальную культуру Китая.</p>
+          </li>
+          <li class="list-info__elem"><img class="elem__icon" src="/public/assets/icons/home/pick-icon.svg"
+              alt="pick icon.">
+            <p class="elem__text">Покупка напрямую от фабрики = экономия в 2-5 раз. Без посредников и скрытых расходов.
+              Чем больше объём мебели в вашем заказе, тем больше ваша выгода!</p>
+          </li>
+        </ul>
+        <!-- TODO: собрать интерьер из фотокарточек -->
+        <img class="buy-info__img" src="/assets/images/home/sofa-comparison-2.png"
+          alt="light sofa with magazine table and armchair.">
+      </div>
       <div></div>
     </div>
   </section>
@@ -49,6 +119,7 @@
   flex-direction: column;
   align-items: center;
   position: relative;
+  gap: 4.4rem;
 }
 
 .home-comparidon__info-block {
@@ -125,5 +196,70 @@
 
 .home-comparison__label__gold {
   color: #ECD180;
+}
+
+.home-comparison__main-block {
+  display: flex;
+  flex-direction: row;
+  gap: 12.4rem;
+  padding-left: 12.5rem;
+}
+
+.main-block__buy-info {
+  width: 55.6rem;
+  height: 76.7rem;
+  border-radius: 4.1rem;
+  background: #3D3D3D;
+  padding: 4.1rem 3.8rem 0 3.4rem;
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+  text-align: start;
+  font-family: Noto Sans;
+  font-size: 1.6rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 130%;
+}
+
+.main-block__buy-info__trips {
+  background: #FFF;
+  border-radius: 2.5rem;
+  width: 56.6rem;
+  color: #333;
+}
+
+.buy-info__list-info {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+  justify-content: start;
+}
+
+.buy-info__label {
+  font-family: Noto Sans;
+  font-size: 2.4rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 130%;
+}
+
+.elem__icon {
+  align-self: start;
+}
+
+.list-info__elem {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: start;
+  gap: 1.1rem;
+}
+
+.buy-info__img {
+  width: 87.2rem;
+  align-self: center
 }
 </style>
